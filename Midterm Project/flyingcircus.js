ed35ca434c6ba4cc03ab9e97b5da3ea38200388c
evinstlaurent <112595836+evinstlaurent@users.noreply.github.com>
2023-11-01 16:57:19 -0400

    midterm project website

--- Midterm Project/flyingcircus.js
+++ Midterm Project/flyingcircus.js
@@ -10,3 +10,42 @@ function dropdownQuestion(answer, type, questionNum) {
 
     document.getElementById("FAQAnswer"+type+questionNum).innerHTML = questionNumHTML;
 }
+
+
+
+//carousel javascript: 
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const carousel = document.querySelector('.carousel');
+    const slides = document.querySelectorAll('.carousel-slide');
+    const prevButton = document.querySelector('.prev-button');
+    const nextButton = document.querySelector('.next-button');
+
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            if (i === index) {
+                slide.style.display = 'block';
+            } else {
+                slide.style.display = 'none';
+            }
+        });
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showSlide(currentIndex);
+    }
+
+    nextButton.addEventListener('click', nextSlide);
+    prevButton.addEventListener('click', prevSlide);
+
+    showSlide(currentIndex);
+});
